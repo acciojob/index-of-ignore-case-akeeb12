0ad86eab5957570ef233ae9c1fb71747a1f19f9d
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-08-17 13:26:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+
+	if (lowerStr && lowerSubStr == 0)
+	{
+		return -1;
+	}
+	
 	let lowerStr = str.toLowerCase();
 	let lowerSubStr = str.toLowerCase();
 
@@ -8,6 +14,6 @@ function indexOfIgnoreCase(s1, s2) {
 }
 
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(indexOfIgnoreCase(s1, s2));
